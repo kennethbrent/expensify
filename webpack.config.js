b6f572badb8c289b4b462ce048f189a6a5cfdf70
kennethbrent <bthomas0144@gmail.com>
2020-04-13 14:30:44 -0400

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+//entry -> output 
+const path = require('path')
+const pathUrl = path.join(__dirname, 'public')
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: pathUrl,
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: pathUrl,
+        historyApiFallback: true
+    }
+}
+
+//loader
+
