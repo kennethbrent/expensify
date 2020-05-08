80ae7fcd27535c2c87f631a3eec2bd666063720d
kennethbrent <bthomas0144@gmail.com>
2020-05-07 23:30:11 -0400

    a

--- src/app.js
+++ src/app.js
@@ -10,18 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = configureStore()
-
-store.dispatch(addExpense({amount: 4500, description: 'Water Bill', createdAt:3000}))
-store.dispatch(addExpense({amount: 8900, description: 'Gas Bill', createdAt:1000}))
-store.dispatch(addExpense({amount: 1095, description: 'Rent', createdAt:12000}))
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-
-console.log(visibleExpenses)
+const store = configureStore();
 const jsx = (
     <Provider store={store}>
         <AppRouter />
